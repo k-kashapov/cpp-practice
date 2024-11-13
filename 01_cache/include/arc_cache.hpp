d8a058f663bea1d89cfcd06518eb21e3508eff06
Kamil Kashapov <kashapov@ispras.ru>
2024-11-13 22:10:26 +0300

    arc cache update: works quicker than a second

--- 01_cache/include/arc_cache.hpp
+++ 01_cache/include/arc_cache.hpp
@@ -4,6 +4,7 @@
 #include <list>
 #include <iostream>
 #include <algorithm>
+#include <unordered_map>
 #include "cache.hpp"
 #include "debug.hpp"
 
@@ -17,18 +18,32 @@ using size_t = std::size_t;
     size_t c_;
     size_t p_;
 
+    struct list_info {
+      size_t list_num;
+      std::list<int>::iterator iter;
+    };
+
+    enum LIST_NUMBER {
+      T1 = 0,
+      T2 = 1,
+      B1 = 2,
+      B2 = 3,
+    };
+
+    std::unordered_map<int, list_info> elem_to_iter;
+
     std::list<int>::iterator find(std::list<int>& where, int elem) {
         return std::find(where.begin(), where.end(), elem);
     }
 
-    void replace(int xt, size_t p);
+    void replace(size_t p, bool in_b2);
 
-    bool case_hit(int elem);
-    bool case_b1(int elem);
-    bool case_b2(int elem);
+    bool case_hit(int elem, list_info& info);
+    bool case_b1 (list_info& info);
+    bool case_b2 (list_info& info);
 
   public:
-    ARCCache(int c) : t1_(), t2_(), b1_(), b2_(), c_(c), p_(0)
+    ARCCache(int c) : t1_(), t2_(), b1_(), b2_(), c_(c), p_(0), elem_to_iter()
     {}
 
     bool AddElem(int elem) override;
--- 01_cache/src/arc_cache.cpp
+++ 01_cache/src/arc_cache.cpp
@@ -1,57 +1,58 @@
 #include <arc_cache.hpp>
 
-void ARCCache::replace(int xt, size_t p) {
-    auto found = find(b2_, xt);
-
-    if ((!t1_.empty()) && ( (t1_.size() > p) || ( (found != b2_.end()) && (t1_.size() == p) ) )) {
+void ARCCache::replace(size_t p, bool in_b2) {
+    if ((!t1_.empty()) && ((t1_.size() > p) || (in_b2 && (t1_.size() == p)))) {
         b1_.splice(b1_.begin(), t1_, std::prev(t1_.end()));
+        elem_to_iter[b1_.front()] = list_info{ B1, b1_.begin() };
     } else {
         b2_.splice(b2_.begin(), t2_, std::prev(t2_.end()));
+        elem_to_iter[b2_.front()] = list_info{ B2, b2_.begin() };
     }
 }
 
-bool ARCCache::case_hit(int elem) {
-    auto found = find(t1_, elem);
-    if (found != t1_.end()) {
+bool ARCCache::case_hit(int elem, list_info& info) {
+    if (info.list_num == T1) {
         // CACHE HIT
         // => move element to MRU (begin) in t2
-        t2_.splice(t2_.begin(), t1_, found);
+        t2_.splice(t2_.begin(), t1_, info.iter);
+        elem_to_iter[elem].list_num = T2;
         return true;
-    } else {
-        found = find(t2_, elem);
-        if (found != t2_.end()) {
-            // CACHE HIT
-            // => move element to MRU (begin) in t2
-            t2_.splice(t2_.begin(), t2_, found);
-            return true;
-        }
+    } else if (info.list_num == T2) {
+        // CACHE HIT
+        // => move element to MRU (begin) in t2
+        t2_.splice(t2_.begin(), t2_, info.iter);
+        return true;
     }
 
     return false;
 }
 
-bool ARCCache::case_b1(int elem) {
-    auto found = find(b1_, elem);
-    if (found != b1_.end()) {
+bool ARCCache::case_b1(list_info& info) {
+    if (info.list_num == B1) {
         // CASE 2 from paper
-        size_t delta = b1_.size() > b2_.size() ? 1 : b2_.size() / b1_.size();
+        size_t delta = b1_.size() >= b2_.size() ? 1 : b2_.size() / b1_.size();
         p_ = std::min(p_ + delta, c_);
-        replace(elem, p_);
-        t2_.splice(t2_.begin(), b1_, found);
+
+        replace(p_, false);
+
+        t2_.splice(t2_.begin(), b1_, info.iter);
+        info.list_num = T2;
         return true;
     }
 
     return false;
 }
 
-bool ARCCache::case_b2(int elem) {
-    auto found = find(b2_, elem);
-    if (found != b2_.end()) {
+bool ARCCache::case_b2(list_info& info) {
+    if (info.list_num == B1) {
         // CASE 3 from paper
-        int delta = b2_.size() > b1_.size() ? 1 : b1_.size() / b2_.size();
+        int delta = b2_.size() >= b1_.size() ? 1 : b1_.size() / b2_.size();
         p_ = std::max((int)p_ - delta, 0);
-        replace(elem, p_);
-        t2_.splice(t2_.begin(), b2_, found);
+        
+        replace(p_, true);
+
+        t2_.splice(t2_.begin(), b2_, info.iter);
+        info.list_num = T2;
         return true;
     }
 
@@ -59,14 +60,20 @@ bool ARCCache::case_b2(int elem) {
 }
 
 bool ARCCache::AddElem(int elem) {
-    if (case_hit(elem)) {
-        debug(std::cout << "ARC HIT elem = " << elem << std::endl);
-        return true;
-    }
+    auto search = elem_to_iter.find(elem);
 
-    if (case_b1(elem) || case_b2(elem)) {
-        debug(std::cout << "case B1 or B2" << std::endl);
-        return false;
+    if (search != elem_to_iter.end()) {
+        auto& info = search->second;
+
+        if (case_hit(elem, info)) {
+            debug(std::cout << "ARC HIT elem = " << elem << std::endl);
+            return true;
+        }
+
+        if (case_b1(info) || case_b2(info)) {
+            debug(std::cout << "case B1 or B2" << std::endl);
+            return false;
+        }
     }
 
     // CASE IV
@@ -76,10 +83,12 @@ bool ARCCache::AddElem(int elem) {
                         << ", t1 size = " << t1_.size() 
                         << ", c_ = " << c_ << std::endl);
         if (t1_.size() < c_) {
+            elem_to_iter.erase(b1_.back());
             b1_.pop_back();
             replace(elem, p_);
         } else {
             debug(std::cout << "t1 back = " << t1_.back() << std::endl);
+            elem_to_iter.erase(t1_.back());
             t1_.pop_back();
         }
     } else {
@@ -88,6 +97,7 @@ bool ARCCache::AddElem(int elem) {
         size_t sum_size = (t1_.size() + t2_.size() + b1_.size() + b2_.size());
         if (sum_size >= c_) {
             if (sum_size == 2 * c_) {
+                elem_to_iter.erase(b2_.back());
                 b2_.pop_back();
             }
 
@@ -98,6 +108,7 @@ bool ARCCache::AddElem(int elem) {
     debug(std::cout << "add " << elem << " to t1 MRU" << std::endl);
 
     t1_.push_front(elem);
+    elem_to_iter[elem] = list_info{T1, t1_.begin()};
 
     return false;
 }
